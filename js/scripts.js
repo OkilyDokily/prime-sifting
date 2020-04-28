f1a366a3a1731911369e6522a36c4926ee2e5b88
me <me@me.me.com>
2020-04-28 15:22:21 -0700

    finish the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
+function primeSift(number){
+  var array = [...Array(number + 1).keys()].splice(1);
+  console.log(array)
+  var prime = 2;
+  while (prime <= number){
+    for(var i = prime - 1; i<array.length; i += prime){
+      if (i == prime - 1){
+        continue;
+      }
+      array[i] = 0;
+    }
+    prime++;
+  }
+  console.log(array);
+  var results = array.filter(x => !(x===0));
+  return results;
+}
+
 $(document).ready(function(){
   $("form").submit(function(e){
-
+    e.preventDefault();
+    var number = parseInt($("input").val());
+    var results = primeSift(number);
+    results.forEach(function(item){
+      $("ul").append("<li>"+ item +"</li>")
+    });
   });
 })
